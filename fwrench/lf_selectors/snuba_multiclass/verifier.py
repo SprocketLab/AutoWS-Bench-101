3672e66e55e96407016518c66a5b1d1615825803
nick11roberts <nick11roberts@cs.wisc.edu>
2022-05-31 18:22:32 -0500

    Update to remove end model

--- fwrench/lf_selectors/snuba_multiclass/verifier.py
+++ fwrench/lf_selectors/snuba_multiclass/verifier.py
@@ -1,65 +1,60 @@
 import numpy as np
 from scipy import sparse
-from .label_aggregator import LabelAggregator
+from snorkel.labeling.model.label_model import LabelModel
+
 
 def odds_to_prob(l):
-  """
+    """
   This is the inverse logit function logit^{-1}:
     l       = \log\frac{p}{1-p}
     \exp(l) = \frac{p}{1-p}
     p       = \frac{\exp(l)}{1 + \exp(l)}
   """
-  return np.exp(l) / (1.0 + np.exp(l))
+    return np.exp(l) / (1.0 + np.exp(l))
+
 
 class Verifier(object):
     """
     A class for the Snorkel Model Verifier
     """
 
-    def __init__(self, L_train, L_val, val_ground, has_snorkel=True):
+    def __init__(self, L_train, L_val, val_ground, classes=10):
         self.L_train = L_train.astype(int)
         self.L_val = L_val.astype(int)
         self.val_ground = val_ground
+        self.classes = classes
 
-        self.has_snorkel = has_snorkel
-        if self.has_snorkel:
-            from snorkel.learning import GenerativeModel
-            from snorkel.learning import RandomSearch
-            from snorkel.learning.structure import DependencySelector
-
-    def train_gen_model(self,deps=False,grid_search=False):
-        """ 
-        Calls appropriate generative model
-        """
-        if self.has_snorkel:
-            #TODO: GridSearch
-            from snorkel.learning import GenerativeModel
-            from snorkel.learning import RandomSearch
-            from snorkel.learning.structure import DependencySelector
-            gen_model = GenerativeModel()
-            gen_model.train(self.L_train, epochs=100, decay=0.001 ** (1.0 / 100), step_size=0.005, reg_param=1.0)
-        else:
-            gen_model = LabelAggregator()
-            gen_model.train(self.L_train, rate=1e-3, mu=1e-6, verbose=False)
+    def train_gen_model(self, deps=False, grid_search=False):
+        gen_model = LabelModel(cardinality=self.classes)
+        gen_model.fit(
+            self.L_train,
+            n_epochs=100,
+            lr=0.005,
+            # decay=0.001 ** (1.0 / 100),
+            # reg_param=1.0,
+        )
         self.gen_model = gen_model
 
     def assign_marginals(self):
         """ 
         Assigns probabilistic labels for train and val sets 
-        """ 
-        self.train_marginals = self.gen_model.marginals(sparse.csr_matrix(self.L_train))
-        self.val_marginals = self.gen_model.marginals(sparse.csr_matrix(self.L_val))
-        #print 'Learned Accuracies: ', odds_to_prob(self.gen_model.w)
+        """
+        self.train_marginals = self.gen_model.predict_proba(self.L_train)
+        self.val_marginals = self.gen_model.predict_proba(self.L_val)
+        # print 'Learned Accuracies: ', odds_to_prob(self.gen_model.w)
 
-    def find_vague_points(self,gamma=0.1,b=0.5):
+    def find_vague_points(self, gamma=0.1, b=0.5):
         """ 
         Find val set indices where marginals are within thresh of b 
         """
-        val_idx = np.where(np.abs(self.val_marginals-b) <= gamma)
+        # TODO argmax multiclass
+        print(self.val_marginals)
+        val_idx = np.where(np.abs(self.val_marginals - b) <= gamma)
         return val_idx[0]
 
-    def find_incorrect_points(self,b=0.5):
+    def find_incorrect_points(self, b=0.5):
         """ Find val set indices where marginals are incorrect """
-        val_labels = 2*(self.val_marginals > b)-1
+        # TODO argmax multiclass
+        val_labels = 2 * (self.val_marginals > b) - 1
         val_idx = np.where(val_labels != self.val_ground)
         return val_idx[0]
